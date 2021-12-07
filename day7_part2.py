bbe905aa5ec84b06bc8636ff0e2add696e41038f
Michelle Havenaar <michellehavenaar@gmail.com>
2021-12-07 20:55:53 +0100

    cleaned up day 7

--- day7_part2.py
+++ day7_part2.py
@@ -1,62 +1,27 @@
-# test = range(0,1968)
-# sumList =sum(test)
-# print(sumList)
-
 from collections import Counter
 
 with open("input/i_day7.txt") as f:
     data = f.read()
 
 data = data.split(",")
-# print(data)
 pos = [int(d) for d in data]
-# print(pos)
 highestPos = max(pos)
-# print(highestPos)
 
 posCounter = Counter(pos)
-# print(posCounter)
 
-#calculate the sum of a range like, if there are 5 steps (1+2+3+4+5)
-#hit limit :( so no recursion
-# def calcSum(n):
-#     if n < 1:
-#         return n
-#     return n + calcSum(n-1)
-
-#print(calcSum(4))
-
-
-# sumList= []
-# for i in range(0, highestPos+1):
-#     sum = 0
-#     # print("target pos: " + str(i))
-#     for p, count in posCounter.items():
-#         #difference between p and targetPos
-#         steps = abs(p - i)
-#         # print(steps)
-#         fuelPerCrab = calcSum(steps)
-#         # print(fuelPerCrab)
-#         fuelPerPos = fuelPerCrab * count
-#         sum += fuelPerPos
-#     sumList.append(sum)
-
-# print(min(sumList))
+def fuelCostToTarget(target, position, count):
+    steps = abs(position - target)
+    fuelList = list(range(1, steps+1))
+    fuelPerCrab = sum(fuelList)
+    fuelPerPos = fuelPerCrab * count
+    return fuelPerPos
 
 sumList= []
 for i in range(0, highestPos+1):
     total = 0
-    # print("target pos: " + str(i))
     for p, count in posCounter.items():
-        #difference between p and targetPos
-        steps = abs(p - i)
-        # print(steps)
-        fuelList = list(range(1,steps+1))
-        # print(fuelList)
-        fuelPerCrab = sum(fuelList)
-        fuelPerPos = fuelPerCrab * count
-        total += fuelPerPos
+        totalFuelperPos = fuelCostToTarget(i, p, count)
+        total += totalFuelperPos
     sumList.append(total)
 
 print(min(sumList))
-
