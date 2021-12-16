2e7d9664990a05269934144c532a7fe517c26933
Michelle Havenaar <michellehavenaar@gmail.com>
2021-12-16 21:42:46 +0100

    day 11 part 1 done

--- day11/day11.py
+++ day11/day11.py
@@ -1,4 +1,6 @@
-with open("day11/i_day11test.txt") as f:
+from collections import deque
+
+with open("day11/i_day11.txt") as f:
     text = f.read()
 
 data = text.split("\n")
@@ -11,188 +13,109 @@ for d in data:
     grid.append(row)
 print(grid)
 
-row = len(grid)
-col = len(grid[0])
+row = len(grid[0])
+col = len(grid)
+
+queue = deque()
 flashCounter = 0
 
-# def incFromNeighbours(x, y, flashCounter):
-#     # nort west = x-1, y-1
-#     if y - 1 >= 0:
-#         # north = x, y-1
-#         neighbour = grid[x][y-1]
-#         if neighbour == 10:
-#             grid[x][y] +=1
-#             if grid[x][y] == 10:
-#                 flashCounter +=1
-#     if x + 1 < len(grid[0])-1 and y - 1 >= 0:
-#         # north east = x+1, y-1
-#         neighbour = grid[x + 1][y - 1]
-#         if neighbour == 10:
-#             grid[x][y] +=1
-#             if grid[x][y] == 10:
-#                 flashCounter +=1
-#     if x + 1 < len(grid[0])-1:
-#         # east = x+1, y
-#         neighbour = grid[x + 1][y]
-#         if neighbour == 10:
-#             grid[x][y] +=1
-#             if grid[x][y] == 10:
-#                 flashCounter +=1
-#     if x + 1 < len(grid[0])-1 and y + 1 < len(grid) -1:
-#         # south east = x+1, y+1
-#         neighbour = grid[x + 1][y + 1]
-#         if neighbour == 10:
-#             grid[x][y] +=1
-#             if grid[x][y] == 10:
-#                 flashCounter +=1
-#     if y + 1 < len(grid)-1:
-#         # south = x, y+1
-#         neighbour = grid[x][y + 1]
-#         if neighbour == 10:
-#             grid[x][y] +=1
-#             if grid[x][y] == 10:
-#                 flashCounter +=1
-#     if x - 1 <= 0 and y + 1 < len(grid)-1:
-#         # south west = x-1, y+1
-#         neighbour = grid[x - 1][y + 1]
-#         if neighbour == 10:
-#             grid[x][y] +=1
-#             if grid[x][y] == 10:
-#                 flashCounter +=1
-#     if x - 1 <= 0:
-#         # west = x-1, y
-#         neighbour = grid[x - 1][y]
-#         if neighbour == 10:
-#             grid[x][y] +=1
-#             if grid[x][y] == 10:
-#                 flashCounter +=1
-#     if x - 1 <= 0 and y - 1 >= 0:
-#         # north west = x-1, y-1
-#         neighbour = grid[x - 1][y - 1]
-#         if neighbour == 10:
-#             grid[x][y] +=1
-#             if grid[x][y] == 10:
-#                 flashCounter +=1
+def flash(i,j):
+    # print(f"flashing for: {i}, {j}")
+    if i - 1 >= 0:
+        # print("N")
+        neighbour = grid[i-1][j]
+        if neighbour <= 9:
+            grid[i-1][j] +=1
+            #if neighbour is now 10 then add it to the queue to look at later
+            if grid[i-1][j] == 10:
+                octo = [i-1,j]
+                queue.append(octo)
+    if i - 1 >= 0 and j + 1 < len(grid[0]):
+        # print("NE")
+        neighbour = grid[i-1][j+1]
+        if neighbour <= 9:
+            grid[i-1][j+1] +=1
+            if grid[i-1][j+1] == 10:
+                octo = [i-1,j+1]
+                queue.append(octo)
+    if j + 1 < len(grid[0]):
+        # print("E")
+        neighbour = grid[i][j+1]
+        if neighbour <= 9:
+            grid[i][j+1] +=1
+            if grid[i][j+1] == 10:
+                octo = [i, j+1]
+                queue.append(octo)
+    if i + 1 < len(grid) and j + 1 < len(grid[0]):
+        # print("SE")
+        neighbour = grid[i + 1][j + 1]
+        if neighbour <= 9:
+            grid[i + 1][j + 1] +=1
+            if grid[i + 1][j + 1] == 10:
+                octo = [i + 1, j + 1]
+                queue.append(octo)
+    if i + 1 < len(grid):
+        # print("S")
+        neighbour = grid[i+1][j]
+        if neighbour <= 9:
+            grid[i+1][j] +=1
+            if grid[i+1][j] == 10:
+                octo = [i+1, j]
+                queue.append(octo)
+    if i + 1 < len(grid) and j - 1 >= 0:
+        # print("SW")
+        neighbour = grid[i + 1][j -1]
+        if neighbour <= 9:
+            grid[i + 1][j -1] +=1
+            if grid[i + 1][j -1] == 10:
+                octo = [i + 1, j -1]
+                queue.append(octo)
+    if j - 1 >= 0:
+        # print("W")
+        neighbour = grid[i][j - 1]
+        if neighbour <= 9:
+            grid[i][j - 1] +=1
+            if grid[i][j - 1] == 10:
+                octo = [i, j - 1]
+                queue.append(octo)
+    if i - 1 >= 0 and j - 1 >= 0:
+        # print("NW")
+        neighbour = grid[i - 1][j - 1]
+        if neighbour <= 9:
+            grid[i - 1][j - 1] +=1
+            if grid[i - 1][j - 1] == 10:
+                octo = [i - 1, j - 1]
+                queue.append(octo)
 
-# grid = [[2, 2, 2, 2, 2], [2, 10, 10, 10, 2], [2, 10, 2, 10, 2], [2, 10, 10, 10, 2], [2, 2, 2, 2, 2]]
-# x = 0
-# y = 0
-# if x + 1 < len(grid[0])-1 and y + 1 < len(grid) -1:
-#     print("true")
-#     neighbour = grid[x + 1][y + 1]
-#     print(neighbour)
-#     if neighbour == 10:
-#         grid[x][y] +=1
-# else:
-#     print("false")
 
-# print(grid)
-# x = 2
-# y = 2
-# if x - 1 >= 0 and y + 1 <= len(grid)-1:
-#     print("true")
-# else: 
-#     print("false")
 
-for step in range(1, 3):
-    print("step: " + str(step))
 
+for step in range(1, 101):
     #step 1
-    for i in range(row):
-        for j in range(col):
-            x = i
-            y = j
-            # value = grid[x][y]
-            grid[x][y] +=1
-            if grid[x][y] == 10:
-                flashCounter +=1
-
-    print(grid)
-    print(flashCounter)
+    for i,row in enumerate(grid):
+        for j,el in enumerate(row):
+            grid[i][j] +=1
 
     #step 2
-    for i in range(row):
-        for j in range(col):
-            x = i
-            y = j
-            # print(x,y)
-            if grid[x][y] != 10:
-                if y - 1 >= 0:
-                    # print("N")
-                    # north = x, y-1
-                    neighbour = grid[x][y-1]
-                    if neighbour == 10:
-                        grid[x][y] +=1
-                        if grid[x][y] == 10:
-                            flashCounter +=1
-                if x + 1 < len(grid[0])-1 and y - 1 >= 0:
-                    # north east = x+1, y-1
-                    # print("NE")
-                    neighbour = grid[x + 1][y - 1]
-                    if neighbour == 10:
-                        grid[x][y] +=1
-                        if grid[x][y] == 10:
-                            flashCounter +=1
-                if x + 1 < len(grid[0])-1:
-                    # east = x+1, y
-                    # print("E")
-                    neighbour = grid[x + 1][y]
-                    if neighbour == 10:
-                        grid[x][y] +=1
-                        if grid[x][y] == 10:
-                            flashCounter +=1
-                if x + 1 < len(grid[0])-1 and y + 1 < len(grid) -1:
-                    # south east = x+1, y+1
-                    # print("SE")
-                    neighbour = grid[x + 1][y + 1]
-                    if neighbour == 10:
-                        grid[x][y] +=1
-                        if grid[x][y] == 10:
-                            flashCounter +=1
-                if y + 1 <= len(grid)-1:
-                    # south = x, y+1
-                    # print("S")
-                    neighbour = grid[x][y + 1]
-                    if neighbour == 10:
-                        grid[x][y] +=1
-                        if grid[x][y] == 10:
-                            flashCounter +=1
-                if x - 1 >= 0 and y + 1 <= len(grid)-1:
-                    # south west = x-1, y+1
-                    # print("SW")
-                    neighbour = grid[x - 1][y + 1]
-                    if neighbour == 10:
-                        grid[x][y] +=1
-                        if grid[x][y] == 10:
-                            flashCounter +=1
-                if x - 1 >= 0:
-                    # west = x-1, y
-                    # print("W")
-                    neighbour = grid[x - 1][y]
-                    if neighbour == 10:
-                        grid[x][y] +=1
-                        if grid[x][y] == 10:
-                            flashCounter +=1
-                if x - 1 >= 0 and y - 1 >= 0:
-                    # north west = x-1, y-1
-                    # print("NW")
-                    neighbour = grid[x - 1][y - 1]
-                    if neighbour == 10:
-                        grid[x][y] +=1
-                        if grid[x][y] == 10:
-                            flashCounter +=1
-
-    print(grid)
-    print(flashCounter)
-
+    for i,row in enumerate(grid):
+        for j,el in enumerate(row):
+            #if 10 then add them to the queue
+            if grid[i][j] == 10:
+                octo = [i, j]
+                queue.append(octo)
+    # take the first from the queue and flash the neighbours
+    while len(queue) != 0:
+        flashingOcto = queue.popleft()
+        flashCounter += 1
+        flash(flashingOcto[0],flashingOcto[1])
+    
     #step 3
-    for i in range(row):
-        for j in range(col):
-            x = i
-            y = j
-            if grid[x][y] == 10:
-                grid[x][y] = 0
+    for i,row in enumerate(grid):
+        for j,el in enumerate(row):
+            if grid[i][j] == 10:
+                grid[i][j]  = 0
 
-    print(grid)
-    print(flashCounter)
+
+print(grid)
+print(flashCounter)
 
