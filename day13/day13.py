25697597b2be440bc82fb0adb305a19b244d431e
Michelle Havenaar <michelle.havenaar2@kpn.com>
2021-12-22 17:23:45 +0100

    day 13 part 1 done

--- day13/day13.py
+++ day13/day13.py
@@ -1,42 +1,57 @@
 with open("day13/i_day13test.txt") as f:
     text = f.read()
 
-# print(text)
-
 data = text.split("\n\n")
-# print(data)
 
 instructions = data[-1]
 instructions = instructions.split("\n")
-# print(instructions)
+
+listOfInstructions = []
+for i in instructions:
+    instr = i.replace("fold along ", "")
+    instruction = instr.split("=")
+    axis = str(instruction[0])
+    value = int(instruction[1])
+    instruction = [axis, value]
+    listOfInstructions.append(instruction)
 
 coords = data[0]
 coords = coords.split("\n")
 coords = [c.split(",") for c in coords]
-# print(coords)
 
-unsortedListOfCoords = []
+listOfCoords = []
 for c in coords:
     co = [int(el) for el in c]
-    unsortedListOfCoords.append(co)
-
-# print(unsortedListOfCoords)
-
-def mergeSort(list):
-    n = len(list)
-    if n == 1: 
-        return
-    else:
-        mid = n //2
-        left = list[: mid]
-        print(left)
-        right = list[mid :]
-        print(right)
-
-        mergeSort(left)
-        mergeSort(right)
+    listOfCoords.append(co)
 
 
-mergeSort(unsortedListOfCoords)
+def mirror(coord, fold, axis):
+    co = coord[axis]
+    dif = co - fold
+    newco = fold - dif
+    coord[axis] = newco
+
+
+def fold(axis, value):
+    if axis == "y":
+        ax = 1
+    if axis == "x":
+        ax = 0
+    for co in listOfCoords:
+        if co[ax] > value:
+            mirror(co, value, ax)
+
+
+firstInst = listOfInstructions[0]
+fold(firstInst[0], firstInst[1])
+
+#remove duplicates
+seen = []
+for co in listOfCoords:
+    if co not in seen:
+        seen.append(co)
+
+print(len(seen))
+
 
 
